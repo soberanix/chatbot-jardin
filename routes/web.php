56cca8f54091c85ea4fb2d67dfbe28b5c911c890
alejandro Trujillo <atsoberanes@gmail.com>
2025-07-07 12:00:39 -0600

    Actializacion de la ruta webhook

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WhatsAppBotController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+
 });
+Route::post('/webhook/whatsapp', [WhatsAppBotController::class, 'handle']);
