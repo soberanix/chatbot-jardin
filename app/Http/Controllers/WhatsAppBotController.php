96eebe4dd3e78c763604f8c2e3f62d62d02a7fc2
alejandro Trujillo <atsoberanes@gmail.com>
2025-07-07 20:01:22 -0600

    empezamos webhok 1

--- app/Http/Controllers/WhatsAppBotController.php
+++ app/Http/Controllers/WhatsAppBotController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Twilio\Rest\Client;
+use App\Models\Paquete;
+use App\Models\Evento;
+use Carbon\Carbon;
 
 class WhatsAppBotController extends Controller
 {
