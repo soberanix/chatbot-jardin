913916b2faf4b23897b4428d067e00ea846e148c
alejandro Trujillo <atsoberanes@gmail.com>
2025-07-07 19:05:31 -0600

    nuevo web 4

--- app/Http/Controllers/WhatsAppBotController.php
+++ app/Http/Controllers/WhatsAppBotController.php
@@ -4,20 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
+use Twilio\Rest\Client;
 
 class WhatsAppBotController extends Controller
 {
     public function handle(Request $request)
     {
         $from = $request->input('From');
-        $body = $request->input('Body');
+        $body = strtolower(trim($request->input('Body')));
 
         Log::info('🔔 Webhook recibido', [
             'from' => $from,
             'body' => $body,
         ]);
 
+        // Respuesta automática
+        $this->responderWhatsApp($from, "Hola 👋 gracias por escribir. ¿En qué puedo ayudarte?");
+
         return response('OK', 200);
     }
-}
 
+    private function responderWhatsApp($to, $mensaje)
+    {
+        $sid = env('TWILIO_ACCOUNT_SID');
+        $token = env('TWILIO_AUTH_TOKEN');
+        $twilio = new Client($sid, $token);
+
+        $twilio->messages->create($to, [
+            'from' => 'whatsapp:' . env('TWILIO_WHATSAPP_NUMBER'),
+            'body' => $mensaje
+        ]);
+    }
+}
